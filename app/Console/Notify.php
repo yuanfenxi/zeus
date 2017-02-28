e2bc096c4f77744b2a767f1373b0156ee7c2a142
黄滚 <renlu.xu@taobao.com>
2017-02-28 18:26:21 +0800

    添加了搜索的监控。

--- app/Console/Notify.php
+++ app/Console/Notify.php
@@ -12,6 +12,6 @@ namespace App\Console;
 trait Notify
 {
     public function sendMsgToStaff($email,$msg){
-        return file_get_contents("https://e.glz8.net/api/wechat/sendMsgToStaff?email={$email}.com&msg=".urlencode($msg));
+        return file_get_contents("https://e.glz8.net/api/wechat/sendMsgToStaff?email={$email}&msg=".urlencode($msg));
     }
 }
